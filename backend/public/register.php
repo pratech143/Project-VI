97c3577bdb01125494320bfdf1a10b73d4b093d4
UnavoidableVOiD <prabhat.211525@ncit.edu.np>
2025-02-15 23:24:12 +0545

    comment lol

--- backend/public/register.php
+++ backend/public/register.php
@@ -5,6 +5,7 @@ include '../config/mail_config.php';
 
 header('Content-Type: application/json');
 
+// Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     echo json_encode(["success" => false, "message" => "Invalid request method. Please use POST."]);
     exit;
